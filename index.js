726256cf07ce9ef5f3f7ef16ce58dffe0fe2b6e9
Carlos E. La Torre <amautacelt@gmail.com>
2021-04-05 18:33:58 -0400

    Done question 5

--- index.js
+++ index.js
@@ -85,6 +85,10 @@ console.log("%c----------", "color: red")
 // *** Uncomment the lines below to test
 console.log("%cQuestion 5", "color: red")
 const student1 = { name: "Duane", grade: 88 }
+
+function updateGrade(student, grade) {
+  student.grade = grade
+}
 updateGrade(student1, 92)
 console.log(student1)
 // // => { name: "Duane", grade: 92 }
